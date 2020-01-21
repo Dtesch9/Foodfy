6198f9928672f3340d38b08b01801c7fe0397c99
Dtesch9 <alucard.s8123@gmail.com>
2020-01-21 14:57:27 -0300

    Add(SQL): new table users

--- foodfydb.sql
+++ foodfydb.sql
@@ -35,6 +35,25 @@ ALTER TABLE "chefs" DROP COLUMN "avatar_url";
 ALTER TABLE "chefs" ADD COLUMN "file_id" INTEGER REFERENCES files(id);
 
 
+-- New users table
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "name" TEXT NOT NULL,
+  "email" TEXT UNIQUE NOT NULL,
+  "password" TEXT NOT NULL,
+  "reset_token" TEXT,
+  "reset_token_expires" TEXT,
+  "is_admin" BOOLEAN DEFAULT false,
+  "created_at" TIMESTAMP DEFAULT(now()),
+  "updated_at" TIMESTAMP DEFAULT(now())
+);
+
+-- Add foreign key on recipes
+ALTER TABLE "recipes" ADD COLUMN "user_id" INTEGER REFERENCES "users" ("id");
+
+
+
+
 -- Procedure and triggers auto update on column updated_at
 CREATE FUNCTION trigger_set_timestamp()
 RETURNS TRIGGER AS $$
@@ -64,9 +83,11 @@ DELETE FROM recipe_files;
 DELETE FROM files;
 DELETE FROM chefs;
 DELETE FROM recipes;
+DELETE FROM users;
 
 -- restart sequence auto_increment from tales ids
 ALTER SEQUENCE chefs_id_seq RESTART WITH 1;
 ALTER SEQUENCE recipes_id_seq RESTART WITH 1;
 ALTER SEQUENCE recipe_files_id_seq RESTART WITH 1;
 ALTER SEQUENCE files_id_seq RESTART WITH 1;
+ALTER SEQUENCE users_id_seq RESTART WITH 1;
