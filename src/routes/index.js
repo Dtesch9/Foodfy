37558a48844628037dc045761818288317808571
Dtesch9 <alucard.s8123@gmail.com>
2020-01-21 15:04:11 -0300

    Feat:(Routes): add users routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,13 @@ const routes = express.Router()
 const foodfy = require('../routes/foodfy')
 const admin = require('../routes/recipes')
 const chefs = require('../routes/chefs')
+const users = require('../routes/users')
 
 
 routes.get('/', (req, res) => res.redirect('/foodfy'))
 
 routes.use('/foodfy', foodfy)
-routes.use('/admin', admin, chefs)
+routes.use('/admin', admin, chefs, users)
 
 // Alias
 routes.get('/admin', (req, res) => res.redirect('/admin/recipes'))
